0b49ad3d717b91157b61a4e96d18fbdeb36253fd
kanika-bhamu <kanika08421202022@msijanakpuri.com>
2023-10-24 21:54:24 +0530

    day-3 24-10-23

---
+++ DSA-Kanika/24-10-23 day-3.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/pascals-triangle/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+        for(int i=0;i<numRows;i++){
+            v[i].push_back(1);
+            for(int j=1;j<i;j++)    v[i].push_back(v[i-1][j]+v[i-1][j-1]);
+            if(i>0)     v[i].push_back(1);
+        }
+        return v;
+    }
+};
