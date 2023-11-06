e97d8ba7bad83650e0edad8406f10ec5834150cd
kanika-bhamu <kanika08421202022@msijanakpuri.com>
2023-11-06 22:51:30 +0530

    day-16 dsa

---
+++ DSA-Kanika/06-11-23 day-16.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+//i have added this so it doesn't show error in my vs code
+//main code is the solution function
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL){
+            inorderTraversal(root->left);
+            ans.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return ans;
+    }
+};
